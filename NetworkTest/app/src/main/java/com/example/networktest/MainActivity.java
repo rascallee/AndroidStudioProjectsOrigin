273a93217e5faac2dc9dc999c682e5eda55a59e5
rascallee <476043129@qq.com>
2019-08-14 10:59:15 +0800

    all commit

--- NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
@@ -60,8 +60,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            //.url("http://www.baidu.com")
-                            .url("http://10.0.2.2/get_data.json")
+                            .url("http://www.baidu.com")
+                            //.url("http://10.0.2.2/get_data.json")
                             .build();
                     Response response = client.newCall(request).execute();//
                     String responseData = response.body().string();
